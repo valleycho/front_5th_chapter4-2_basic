3e0ecd6ad851919345db0a6e35372e7a609603ff
ssp <qkrtjdtlr02@naver.com>
2025-06-02 21:50:19 +0900

    favicon 및 best practices에서 출력하는 에러 allow origin 개선

--- js/products.js
+++ js/products.js
@@ -1,5 +1,10 @@
 async function loadProducts() {
-  const response = await fetch("https://fakestoreapi.com/products");
+  const response = await fetch("https://fakestoreapi.com/products", {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
   const products = await response.json();
   displayProducts(products);
 }
